4809edbe96d84b9c02ab4c8bc460044cdc5272f1
Zuson <1244923939@qq.com>
2018-07-30 18:34:21 +0800

    获取、增加、删除文章的接口

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,38 @@ var router = express.Router();
 const models = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
-});
+})
 
-router.get('/test-api', (req, res, next) => {
-	res.json({ title: '0910' });
-});
+/* 获取所有文章 */
+router.get('/get-articles', (req, res) => {
+  models.Article.findAll().then(articles => {
+    res.json({ results: articles })
+  })
+})
 
-module.exports = router;
+/* 新增一篇文章 */
+router.post('/add-articles', (req, res) => {
+  models.Article.create(req.body).then(article => {
+    res.json({ status: 0 })
+  })
+})
+
+/* 指定删除一篇文章 */
+router.post('/remove-articles', (req, res) => {
+  models.Article
+    .findById(req.body.id)
+    .then(article => {
+      if (!article) return res.json({ ret: 400, results: 'Article Not Found.' })
+      article
+        .destroy()
+        .then(() => res.json({ ret: 0, results: 'success' }))
+        .catch(err => res.json({ ret: 400, results: 'error1' }))
+    })
+    .catch(err => {
+      res.json({ ret: 400, results: 'error2' })
+    })
+})
+
+module.exports = router
